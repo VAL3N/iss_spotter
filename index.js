0434f800ec2a38c74aa92825dd8228e9963c8a9d
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-05 16:07:59 +0000

    modified

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
+};
 
-  console.log('It worked! Returned IP:' , ip);
-
-  fetchCoordsByIP(ip, (error, coordinates) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(coordinates);
-    }
-  });
-
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
-
-
-
-
-/* ***** Setting the callback into a variable to be used ****** */
-// const callback = (error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-// };
-
-// fetchMyIP(callback);
-
